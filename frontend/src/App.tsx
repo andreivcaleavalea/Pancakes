b62871fbf2fa3c5582453cb3c053628a0c34971c
andreivcaleavalea <andrei.v.caleavalea@gmail.com>
2025-07-14 16:50:57 +0300

    Declared global color scheme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import { ConfigProvider } from 'antd';
+import { App as AntdApp, ConfigProvider } from 'antd';
 import MainLayout from './layouts/MainLayout/MainLayout';
 import HomePage from './pages/HomePage/HomePage';
 import './styles/globals.scss';
 import './App.css';
 
-const theme = {
-  token: {
-    colorPrimary: '#E9833A', // Maple syrup orange
-    colorSuccess: '#B16B2F', // Brown sugar
-    colorWarning: '#FFB549', // Butter yellow
-    colorInfo: '#F5DEB3', // Wheat/pancake color
-    colorError: '#D14B1F', // Cinnamon
-    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
-  },
-};
-
 const App: React.FC = () => {
   return (
-    <ConfigProvider theme={theme}>
-      <MainLayout>
-        <HomePage />
-      </MainLayout>
+    <ConfigProvider>
+      <AntdApp>
+        <MainLayout>
+          <HomePage />
+        </MainLayout>
+      </AntdApp>
     </ConfigProvider>
   );
 };
