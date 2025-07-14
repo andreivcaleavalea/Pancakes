e8ae8fdf76d4e0e0d74ea8996e7f0b556389293a
andreivcaleavalea <andrei.v.caleavalea@gmail.com>
2025-07-14 14:44:14 +0300

    Removed tags

--- frontend/src/types/blog.ts
+++ frontend/src/types/blog.ts
@@ -4,14 +4,7 @@ export interface BlogPost {
   description: string;
   date: string;
   image: string;
-  tags: Tag[];
   author?: string;
-}
-
-export interface Tag {
-  name: string;
-  color: string;
-  backgroundColor: string;
 }
 
 export interface FeaturedPost extends BlogPost {
